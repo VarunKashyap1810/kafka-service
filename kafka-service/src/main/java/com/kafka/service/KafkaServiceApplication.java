34f041d399f82e3186ac2244d0d60b25cd981a5e
Varun.Kashyap <varunkashyap1810@gmail.com>
2022-03-29 19:52:10 +0530

    Kafka With Spring Boot. Producer and Consumer

---
+++ kafka-service/src/main/java/com/kafka/service/KafkaServiceApplication.java
@@ -0,0 +1,14 @@
+package com.kafka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaServiceApplication.class, args);
+		System.out.println(11);
+	}
+
+}
